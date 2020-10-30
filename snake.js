c6745e048b7426437e6b3d3711aaa5c2892c5d79
Nashila <nashilaamod@gmail.com>
2020-10-30 03:08:38 +0100

    Extensions : Add 3 game modes, move food on the grid, game over interface, sounds effects

--- snake.js
+++ snake.js
@@ -1,16 +1,31 @@
 let world = [];
-var ROWS = 40, COLS = 80;
+var ROWS, COLS;
 var EMPTY = "EMPTY", SNAKE = "SNAKE", FRUIT = "FRUIT";
 
 let snake = [];
+var snakeLength;
+var snakeHeadPosition = [];
 const LEFT = 0, UP = 1, RIGHT = 2, DOWN = 3, NONE = -1;
 let direction = NONE;
 
+var initialFoodPosition = [];
+var foodMove;
+var foodDelay;
+
 var canvas,	canvas_context;
 var gameActive, gameControl;
-var x = 200; // call step function X times per second
+var delay; // call step function X times per second
 var score;
 var level;
+
+var dataJson;
+
+// Audio files
+const deadAudio = new Audio();
+const eatAudio = new Audio();
+
+deadAudio.src = "dead.mp3";
+eatAudio.src = "eat.mp3";
 
 
 /* CREATION OF THE WORLD */
@@ -56,15 +71,15 @@ function draw()
             switch (getCellWorld(x,y)) {
                 
 				case EMPTY:
-					canvas_context.fillStyle = "#0ff";
+					canvas_context.fillStyle = "#292b2c";
                     break;
                     
 				case SNAKE:
-                    canvas_context.fillStyle = "#ff0000";
+                    canvas_context.fillStyle = "#f7f7f7";
                     break;
                 
                 case FRUIT :
-                    canvas_context.fillStyle = "#000";
+                    canvas_context.fillStyle = "#5cb85c";
 					break;
 			}
 
@@ -73,7 +88,7 @@ function draw()
     }
 
     // changes the fillstyle once more and draws the score
-    canvas_context.fillStyle = "#000";
+    canvas_context.fillStyle = "#f7f7f7";
 	canvas_context.font = "15px Helvetica";
 	canvas_context.fillText("SCORE : " + score, canvas.width/2, canvas.height-10);
 }
@@ -87,7 +102,7 @@ function createSnake(x_row,y_col, snakeLength)
 
     for(var x = 1; x < snakeLength ; x++)
     {
-        x_row++;
+        y_col++;
         grow(x_row, y_col);
     }
 }
@@ -101,7 +116,7 @@ function insertHead(x_row, y_col)
 function grow(x_row, y_col)
 {
     setCellWorld(SNAKE, x_row, y_col);
-    return snake.unshift(x_row, y_col);
+    return snake.unshift([x_row, y_col]);
 }
 
 function removeTail(x_row,y_col)
@@ -113,7 +128,13 @@ function removeTail(x_row,y_col)
 
 /* GENERATING FOOD */
 
-function setFood() {
+function setInitialFood(x_row, y_col)
+{
+    setCellWorld(FRUIT, x_row, y_col);
+    foodMove = setTimeout(function () { removeFood(x_row, y_col);}, foodDelay);    
+}
+
+function setFoodRandom() {
 
     var empty = [];
     
@@ -131,7 +152,24 @@ function setFood() {
     
 	// chooses a random cell
     var fruitCell = empty[Math.round(Math.random()*(empty.length - 1))];
-    setCellWorld(FRUIT, fruitCell[0], fruitCell[1]);
+    var x_row = fruitCell[0];
+    var y_col = fruitCell[1];
+    setCellWorld(FRUIT, x_row, y_col);
+
+    foodMove = setTimeout(function () { removeFood(x_row, y_col);}, foodDelay);
+
+}
+
+function removeFood(x_row, y_row)
+{    
+    
+    if(getCellWorld(x_row, y_row) === FRUIT)
+    {
+        setCellWorld(EMPTY, x_row, y_row);
+        setFoodRandom();
+        return;
+    }
+  
 }
 
 
@@ -140,10 +178,11 @@ function setFood() {
 function startMenu()
 {
     var levelSelect = document.getElementById("levelSelect");
-    niveau = levelSelect.value;
+    level = levelSelect.value;
     
     var startMenuDiv = document.getElementById("startMenu");
     startMenuDiv.parentNode.removeChild(startMenuDiv);
+    initiateLevel();
     initGame();
 }
 
@@ -161,42 +200,97 @@ function initGame()
 
     document.addEventListener("keydown", keyDownEvent);
 
-    gameControl = startGame(x);
+    gameControl = startGame(delay);
 }
 
-function startGame(x)
+function startGame(delay)
 {
     gameActive = true;
     init();
-    draw();
-    return setInterval(step, x);
+    return setInterval(step, delay);
 }
 
 function init() 
 {
-    // Initiate the game
     createWorld(ROWS, COLS);
     score = 0;
 
     // Initiate the snake
-    var snakeX = Math.floor(ROWS/2);
-    var snakeY = Math.floor(COLS/ 2);
-    createSnake(snakeX - 1, snakeY - 1, 1);
+    var snakeX = snakeHeadPosition[0];
+    var snakeY = snakeHeadPosition[1];
+    createSnake(snakeX, snakeY, snakeLength);
 
     // Set food on the grid
-    setFood();
+    setInitialFood(initialFoodPosition[0], initialFoodPosition[1]);
+    draw();
+}
+
+function initiateLevel()
+{
+    var request = new XMLHttpRequest();
+
+    // Initiate the game
+    if(level === "level1")
+    {
+        request.open("GET", "../level1.json", false);
+        request.send(null)
+        dataJson = JSON.parse(request.responseText);
+    }
+    else if(level === "level2")
+    {
+        request.open("GET", "../level2.json", false);
+        request.send(null)
+        dataJson = JSON.parse(request.responseText);
+    }
+    else if(level === "level3")
+    {
+        request.open("GET", "../level3.json", false);
+        request.send(null)
+        dataJson = JSON.parse(request.responseText);
+    }
+
+    ROWS = dataJson.dimensions[1];
+    COLS = dataJson.dimensions[0];
+    delay = dataJson.delay;
+    foodDelay = dataJson.foodDelay;
+
+    snakeLength = dataJson.snake.length;
+    snakeHeadPosition = Array.from(dataJson.snake[snakeLength-1]);
+
+    initialFoodPosition = Array.from(dataJson.food[0]);
 }
 
 function endGame() 
 {
-    clearInterval(gameControl);
+    clearInterval(gameControl);  
+
     gameActive = false;
 
-    canvas_context.fillStyle = 'white';
-    canvas_context.textBaseline = 'middle'; 
-    canvas_context.textAlign = 'center'; 
-    canvas_context.font = 'normal bold 18px serif';
-    canvas_context.fillText('Game over', canvas.width/2, canvas.height/2);
+    canvas.parentNode.removeChild(canvas);
+
+    var divGameOver = document.createElement("div");
+    divGameOver.classList += "text-white bg-dark text-center border rounded p-5 w-50"
+
+    var titleGameOver = document.createElement("h1");
+    titleGameOver.classList += "my-5";
+    titleGameOver.textContent = 'GAME OVER';
+
+    var divButton = document.createElement("div");
+    divButton.classList += "d-flex justify-content-center";
+
+    var refreshButton = document.createElement("button");
+    refreshButton.classList += "btn btn-danger btn-lg mb-5";
+    refreshButton.setAttribute('type', 'button');
+    refreshButton.setAttribute('onclick', "window.location.reload();");
+    refreshButton.textContent = "RESTART";
+
+    divButton.appendChild(refreshButton);
+    divGameOver.appendChild(titleGameOver);
+    divGameOver.appendChild(divButton);
+
+    document.body.appendChild(divGameOver);
+
+    deadAudio.play();
 
 }
 
@@ -250,69 +344,73 @@ function keyDownEvent(e) {
 
 function move_snake()
 {
-    // Get the position of the head :
-    var head = snake[snake.length - 1]
-    var headX = head[0];
-    var headY = head[1];
-
-    switch (direction) {
-
-        case LEFT:
-            headY--; //decrease column
-            break;
-
-        case UP:
-            headX--; //decrease row
-            break;
-
-        case RIGHT:
-           headY++;
-            break;
-
-        case DOWN:
-            headX++;
-            break;
-    }
-
-    // checks all gameover conditions
-
-    if (0 > headX || headX > ROWS-1) 
+    if(direction != NONE)
     {
-        gameActive = false;
-        return;
-    }
+        // Get the position of the head :
+        var head = snake[snake.length - 1]
+        var headX = head[0];
+        var headY = head[1];
 
-    if (0 > headY || headY > COLS-1) 
-    {
-        gameActive = false;
-        return;
-    }
+        switch (direction) {
 
-    if(getCellWorld(headX, headY) === SNAKE && snake.length > 1)
-    {
-        gameActive = false;
-        return;
+            case LEFT:
+                headY--; //decrease column
+                break;
+
+            case UP:
+                headX--; //decrease row
+                break;
+
+            case RIGHT:
+                headY++;
+                break;
+
+            case DOWN:
+                headX++;
+                break;
+        }
+
+        //checks all gameover conditions
+
+        if (0 > headX || headX > ROWS-1) 
+        {
+            gameActive = false;
+            return;
+        }
+
+        if (0 > headY || headY > COLS-1) 
+        {
+            gameActive = false;
+            return;
+        }
+
+        if(getCellWorld(headX, headY) === SNAKE && snake.length > snakeLength)
+        {
+            gameActive = false;
+            return;
+        }
+
+        // check whether the new position are on the fruit item
+
+        if (getCellWorld(headX, headY) === FRUIT) 
+        {
+            score++;
+            eatAudio.play();
+            setFoodRandom();            
+        } 
+        else 
+        {
+            // Get the position of the tail
+            var tail = snake[0];
+            var tailX = tail[0];
+            var tailY = tail[1];
+
+            // Remove the last position of the tail
+            removeTail(tailX, tailY);
+        }
+
+        // Add new head 
+        insertHead(headX, headY);
     }
     
-
-    // check whether the new position are on the fruit item
-
-    if (getCellWorld(headX, headY) === FRUIT) 
-    {
-        score++;
-        setFood();
-    } 
-    else 
-    {
-        // Get the position of the tail
-        var tail = snake[0];
-        var tailX = tail[0];
-        var tailY = tail[1];
-
-        // Remove the last position of the tail
-        removeTail(tailX, tailY);
-    }
-
-    // Add new head 
-    insertHead(headX, headY);
 }
